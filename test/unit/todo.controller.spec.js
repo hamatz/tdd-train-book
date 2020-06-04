6c8b4e3fefd5b9f2a0548582b2bddbe6d1694d22
hamatz <hamatsu.m@googlemail.com>
2020-06-04 23:26:09 +0900

    プログラム側の更新

--- test/unit/todo.controller.spec.js
+++ test/unit/todo.controller.spec.js
@@ -1,7 +1,42 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
+
+TodoModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = jest.fn();
+});
 
 describe("TodoController.create", () => {
-  it("should have a create function", () => {
-    expect(typeof TodoController.create).toBe("function");
+  beforeEach(() => {
+    req.body = newTodo;
+  });
+
+  it("should Call TodoModel.create", () => {
+    TodoController.create(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+  it("should return 201 response code", async () => {
+    await TodoController.create(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+  it("should return json body in response", async () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    await TodoController.create(req, res, next);
+    expect(res._getData()).toStrictEqual(newTodo);
+  });
+  it("shoul handle model validation errors", async () => {
+    const errorMessage = { message: "Done property missing" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    TodoModel.create.mockReturnValue(rejectedPromise);
+    await TodoController.create(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
   });
 });
+
